0bf3c5f5a8567c1c08911ef1fbe9827ea1bc70f5
TheRealRomy <100058219+TheRealRomy@users.noreply.github.com>
2023-07-20 13:50:14 +0800

    Made the animations for the female knight.
    Slightly optimized the player movement script. Thinking of going manual level design instead of procedurally generated.

--- Dungeon-Crawler/Assets/Scripts/Player Movement.cs
+++ Dungeon-Crawler/Assets/Scripts/Player Movement.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float movementSpeed = 5f;
+    private Vector3 movementDirection;
+    private Animator animator;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        float yMovement = Input.GetAxisRaw("Vertical");
+
+        movementDirection = new Vector3(xMovement, yMovement, 0).normalized;
+
+        if (movementDirection.sqrMagnitude > 0)
+        {
+            if (xMovement > 0) 
+            {
+                transform.localScale = Vector3.one;
+            }
+            else if (xMovement < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            animator.SetBool("isMoving", true);
+            animator.SetFloat("xMovement", xMovement);
+            animator.SetFloat("yMovement", yMovement);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+            animator.SetFloat("xMovement", xMovement);
+            animator.SetFloat("yMovement", yMovement);
+        }
+
+        transform.Translate(movementDirection * movementSpeed * Time.deltaTime);
     }
 }
