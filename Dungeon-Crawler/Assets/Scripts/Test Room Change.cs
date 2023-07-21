5692f39adcab13a11183794106779eed392bc8bd
TheRealRomy <100058219+TheRealRomy@users.noreply.github.com>
2023-07-21 15:53:21 +0800

    Made 2 camera and map types.

---
+++ Dungeon-Crawler/Assets/Scripts/Test Room Change.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRoomChange : MonoBehaviour
+{
+    public GameObject virtualCamera;
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player") && !other.isTrigger) {
+            virtualCamera.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Player") && !other.isTrigger) {
+            virtualCamera.SetActive(false);
+        }
+    }
+}
