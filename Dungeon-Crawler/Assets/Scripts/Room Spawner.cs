0aadbea02f46fcbe7fe05960c97d064c82cf87be
TheRealRomy <100058219+TheRealRomy@users.noreply.github.com>
2023-07-23 21:01:29 +0800

    Save point of the room generation

---
+++ Dungeon-Crawler/Assets/Scripts/Room Spawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirectionValue; 
+
+    private RoomTemplate roomTemplate;
+    private int random;
+    private bool roomHasSpawned = false;
+
+    private void Start() {
+        roomTemplate = GameObject.FindGameObjectWithTag("Room Template").GetComponent<RoomTemplate>();
+        Invoke("spawnRoom", .2f);
+    }
+
+    void spawnRoom()
+    {
+        if (roomHasSpawned == false) 
+        {
+            if (openingDirectionValue == 1) {
+                random = Random.Range(0, roomTemplate.bottomRoom.Length);
+                Instantiate(roomTemplate.bottomRoom[random], transform.position, roomTemplate.bottomRoom[random].transform.rotation);
+            }
+            else if (openingDirectionValue == 2) {
+                random = Random.Range(0, roomTemplate.topRoom.Length);
+                Instantiate(roomTemplate.topRoom[random], transform.position, roomTemplate.topRoom[random].transform.rotation);
+            }
+            else if (openingDirectionValue == 3) {
+                random = Random.Range(0, roomTemplate.leftRoom.Length);
+                Instantiate(roomTemplate.leftRoom[random], transform.position, roomTemplate.leftRoom[random].transform.rotation);
+            }
+            else if (openingDirectionValue == 4) {
+                random = Random.Range(0, roomTemplate.rightRoom.Length);
+                Instantiate(roomTemplate.rightRoom[random], transform.position, roomTemplate.rightRoom[random].transform.rotation);
+            }
+            roomHasSpawned = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Spawn Point")){
+            if (other.GetComponent<RoomSpawner>().roomHasSpawned == false && roomHasSpawned == false) {
+                Instantiate(roomTemplate.closedRoom, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            roomHasSpawned = true;
+        }
+    }
+}
---
+++ Dungeon-Crawler/Assets/Scripts/Room Template.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplate : MonoBehaviour
+{
+    public GameObject[] topRoom;
+    public GameObject[] bottomRoom;
+    public GameObject[] leftRoom;
+    public GameObject[] rightRoom;
+    public GameObject closedRoom;
+}
